9d4fe150e7f56d2c8bcba31e2563bc31b75bbb98
Valeri Mladenov <valeri.mladenov10@gmail.com>
2020-03-15 01:22:45 +0200

    Apollo & GraphQL Implemented

---
+++ src/ApolloProvider.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from '@apollo/react-hooks';
+import App from './App';
+
+const cache = new InMemoryCache();
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : '',
+    },
+  };
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+  },
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+});
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
